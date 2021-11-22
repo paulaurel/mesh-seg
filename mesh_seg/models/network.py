fbd1896261e2e63bc424289f7c13269b9dbd8a98
Paul Aurel Diederichs <paulaureldiederichs@gmail.com>
2021-11-22 14:25:11 -0800

    ADD: Add mesh correspondence network

--- mesh_seg/models/network.py
+++ mesh_seg/models/network.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from ..utils import pairwise
+from .sinkhorn import solve_log_optimal_transport
 from .graph_convolutions.feat_steer_conv import FeatureSteeredConvolution
 
 
@@ -98,3 +99,68 @@ class MeshSeg(torch.nn.Module):
         x = self.input_encoder(x)
         x = self.gnn(x, edge_index)
         return self.final_projection(x)
+
+
+class MeshCorrSeg(torch.nn.Module):
+    def __init__(
+            self,
+            in_features: int,
+            num_seg_classes: int,
+            encoder_channels: list,
+            conv_channels: list,
+            class_decoder_channels: list,
+            assignment_decoder_channels: list,
+            num_heads: int,
+            apply_batch_norm: bool = True,
+            sinkhorn_iterations: int = 5,
+    ):
+        super().__init__()
+        self.sinkhorn_iterations = sinkhorn_iterations
+
+        *_, final_conv_channel = conv_channels
+        *_, final_encoder_channel = encoder_channels
+
+        self.input_encoder = get_mlp_layers(
+            channels=[in_features] + encoder_channels,
+            activation=nn.ReLU)
+        self.gnn = GraphFeatureEncoder(
+            in_features=final_encoder_channel,
+            conv_channels=conv_channels,
+            num_heads=num_heads,
+            apply_batch_norm=apply_batch_norm,
+        )
+        self.class_projection = get_mlp_layers(
+            channels=[final_conv_channel] + class_decoder_channels + [num_seg_classes],
+            activation=nn.ReLU,
+        )
+        self.assignment_projection = get_mlp_layers(
+            channels=[final_conv_channel] + assignment_decoder_channels,
+            activation=nn.ReLU,
+        )
+
+    def _compute_embedding(self, x, edge_index):
+        return self.gnn(self.input_encoder(x), edge_index)
+
+    def _compute_assignment(self, emb_s, emb_t):
+        assignment_matrix = torch.einsum(
+            'nd, md -> nm', self.assignment_projection(emb_s), self.assignment_projection(emb_t)
+        )
+        num_assignments = emb_s.shape[-1]
+        assignment_matrix /= num_assignments ** 0.5
+        assignment_matrix = solve_log_optimal_transport(
+            assignment_matrix,
+            num_iterations=self.sinkhorn_iterations,
+        )
+        return assignment_matrix
+
+    def _compute_class_labels(self, emb_s, emb_t):
+        return self.class_projection(emb_s), self.class_projection(emb_t)
+
+    def forward(self, data_s, data_t):
+        embeddings = [
+            self._compute_embedding(x, edge_index)
+            for x, edge_index in ((data_s.x, data_s.edge_index), (data_t.x, data_t.edge_index))
+        ]
+        assignment_matrix = self._compute_assignment(*embeddings)
+        class_labels_s, class_labels_t = self._compute_class_labels(*embeddings)
+        return assignment_matrix, class_labels_s, class_labels_t
---
+++ mesh_seg/models/sinkhorn.py
@@ -0,0 +1,26 @@
+import torch
+
+
+def perform_log_sinkhorn_iterations(log_assignment_matrix, log_mu, log_nu, num_iterations: int):
+    """Perform Sinkhorn Normalization in Log-space for stability"""
+    u, v = torch.zeros_like(log_mu), torch.zeros_like(log_nu)
+    for _ in range(num_iterations):
+        u = log_mu - torch.logsumexp(log_assignment_matrix + v.unsqueeze(1), dim=2)
+        v = log_nu - torch.logsumexp(log_assignment_matrix + u.unsqueeze(2), dim=1)
+    return log_assignment_matrix + u.unsqueeze(2) + v.unsqueeze(1)
+
+
+def solve_log_optimal_transport(assignment_matrix, num_iterations: int):
+    """Perform Differentiable Optimal Transport in Log-space for stability"""
+    batch_size, m, n = assignment_matrix.shape
+    scalar_one = torch.tensor(1, dtype=assignment_matrix.dtype)
+    ms, ns = (m * scalar_one).to(assignment_matrix), (n * scalar_one).to(assignment_matrix)
+
+    norm = - (ms + ns).log()
+    log_mu = torch.cat([norm.expand(m), ns.log()[None] + norm])
+    log_nu = torch.cat([norm.expand(n), ms.log()[None] + norm])
+    log_mu, log_nu = log_mu[None].expand(batch_size, -1), log_nu[None].expand(batch_size, -1)
+
+    log_assignment_matrix = perform_log_sinkhorn_iterations(assignment_matrix, log_mu, log_nu, num_iterations)
+    log_assignment_matrix = log_assignment_matrix - norm
+    return log_assignment_matrix
