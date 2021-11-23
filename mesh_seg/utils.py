03fb0bb1386e4e4a6d0c59afaf4cbf4e49711605
Paul Aurel Diederichs <paulaureldiederichs@gmail.com>
2021-11-22 17:11:59 -0800

    ADD: Add groupwise iteration

--- mesh_seg/utils.py
+++ mesh_seg/utils.py
@@ -2,7 +2,7 @@ from itertools import tee
 
 
 def pairwise(iterable):
-    """Iterate over all pairs of consecutive items in a list.
+    """Iterate over all pairs of consecutive items in an iterable.
 
     Notes
     -----
@@ -11,3 +11,19 @@ def pairwise(iterable):
     a, b = tee(iterable)
     next(b, None)
     return zip(a, b)
+
+
+def groupwise(iterable, group_size=2):
+    """Iterate over all groups of consecutive items in an iterable.
+
+    Notes
+    -----
+        groupwise([s0, s1, s2, s3, ...], group_size=2) -> (s0, s1), (s2, s3), ...
+        groupwise([s0, s1, s2, s3, ...], group_size=3) -> (s0, s1, s2) (s3, s4, s5), ...
+    """
+    iterable = iter(iterable)
+    while True:
+        try:
+            yield tuple(next(iterable) for _ in range(group_size))
+        except RuntimeError:
+            return
